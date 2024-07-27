82af808aa6f251740fc68512485b1e7630417e0e
Malik Arrosid <malikarrosid.md@gmail.com>
2024-07-27 19:35:16 +0900

    Update client.dart

--- lib/client.dart
+++ lib/client.dart
@@ -5,7 +5,7 @@ import 'package:news_app/response_articles.dart';
 class Client {
   static Future<List<Article>> fetchArticle() async {
     const url =
-        "https://newsapi.org/v2/everything?q=Indonesia&sources?country=id&apiKey=9fa5cce232174854abc46c38c8b279af";
+        "https://newsapi.org/v2/everything?q=Indonesia&sources?country=id&apiKey=YOUR_API_KEY";
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
